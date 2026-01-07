cbda461c814e4376484cdd574c52856bd42f68a3
Frederico Mazzo <fredmazzo@gmail.com>
2026-01-07 13:44:32 -0300

    fix: Adicionar try/catch geral no chatWithAssistant para capturar todos os erros

--- server/routers.ts
+++ server/routers.ts
@@ -3641,15 +3641,16 @@ ${personalityDescriptions[input.personality || 'professional']}
         collectedInfo: z.record(z.any()).optional(), // Informações coletadas
       }))
       .mutation(async ({ ctx, input }) => {
-        console.log('[ChatWithAssistant] 📥 Recebida requisição:', {
-          agentId: input.agentId,
-          conversationId: input.conversationId,
-          messagesCount: input.messages?.length || 0,
-          userName: input.userName,
-          hasCollectedInfo: !!input.collectedInfo,
-        });
+        try {
+          console.log('[ChatWithAssistant] 📥 Recebida requisição:', {
+            agentId: input.agentId,
+            conversationId: input.conversationId,
+            messagesCount: input.messages?.length || 0,
+            userName: input.userName,
+            hasCollectedInfo: !!input.collectedInfo,
+          });
 
-        let tenant = ctx.tenant;
+          let tenant = ctx.tenant;
         
         if (!tenant && ctx.user) {
           tenant = await db.getTenantByUserId(ctx.user.id);
